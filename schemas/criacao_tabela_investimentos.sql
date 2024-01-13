50a1f442c0888a4805666fb587252595156665de
Matheus Rodrigues <matheussr61@gmail.com>
2024-01-13 16:00:08 -0300

    add schemas folder

---
+++ schemas/criacao_tabela_investimentos.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS estados (
+	estado_key INT,
+	nome VARCHAR(30) NOT NULL,
+	PRIMARY KEY (estado_key)
+);
+
+CREATE TABLE IF NOT EXISTS datas (
+	data_key INT,
+	ano integer NOT NULL,
+	PRIMARY KEY (data_key)
+);
+
+CREATE TABLE IF NOT EXISTS subfuncoes (
+	subfuncoes_key INT,
+	nome VARCHAR(30) NOT NULL,
+	PRIMARY KEY (subfuncoes_key)
+);
+
+CREATE TABLE IF NOT EXISTS investimentos (
+	investimento_key INT,
+	montante NUMERIC NOT NULL,
+	estado INT NOT NULL,
+	"data" INT NOT NULL,
+	subfuncao INT NOT NULL,
+	PRIMARY KEY (investimento_key),
+	FOREIGN KEY (estado)
+		REFERENCES estados (estado_key),
+	FOREIGN KEY ("data")
+		REFERENCES datas (data_key),
+	FOREIGN KEY (subfuncao)
+		REFERENCES subfuncoes (subfuncoes_key)
+);
